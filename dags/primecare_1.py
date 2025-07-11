621c155c1c8e58fa3ac3bb5829aeb3d3218c52a6
LinaresJ <linares.j@alterra-ds.com>
2025-07-10 21:02:43 -0500

    Refactor upload tasks in primecare_1.py to use parquet files and update container names

--- dags/primecare_1.py
+++ dags/primecare_1.py
@@ -1,6 +1,6 @@
 import os
 from airflow import DAG
-from datetime import datetime
+from datetime import datetime, timedelta
 from airflow.providers.microsoft.azure.transfers.local_to_wasb import LocalFilesystemToWasbOperator
 
 with DAG(
@@ -13,13 +13,37 @@ with DAG(
 ) as dag:
 
     upload_task = LocalFilesystemToWasbOperator(
-    task_id='upload_files',
-    file_path='/opt/airflow/data/claims.csv',
-    container_name='bronze',
-    blob_name='claims/{{ ds }}/claims.csv',  # Particionado por fecha
+    task_id='upload_claims',
+    file_path='/opt/airflow/data/claims.parquet',
+    container_name='datalake',
+    blob_name='raw/airflow/G3/claims.parquet',  # Particionado por fecha
     wasb_conn_id='azureblob_wasb',
     create_container=True,
-    retries=3,                               # Reintentos
-    retry_delay=timedelta(minutes=5),        # Delay entre reintentos
-    execution_timeout=timedelta(minutes=30)  # Timeout
+    # retries=3,                               # Reintentos
+    # retry_delay=timedelta(minutes=5),        # Delay entre reintentos
+    # execution_timeout=timedelta(minutes=30)  # Timeout
+    )
+
+    upload_task = LocalFilesystemToWasbOperator(
+    task_id='upload_patients',
+    file_path='/opt/airflow/data/patients.parquet',
+    container_name='datalake',
+    blob_name='raw/airflow/G3/patients.parquet',  # Particionado por fecha
+    wasb_conn_id='azureblob_wasb',
+    create_container=True,
+    # retries=3,                               # Reintentos
+    # retry_delay=timedelta(minutes=5),        # Delay entre reintentos
+    # execution_timeout=timedelta(minutes=30)  # Timeout
+    )
+
+    upload_task = LocalFilesystemToWasbOperator(
+    task_id='upload_CIE10',
+    file_path='/opt/airflow/data/cie10_catalog.parquet',
+    container_name='datalake',
+    blob_name='raw/airflow/G3/cie10_catalog.parquet',  # Particionado por fecha
+    wasb_conn_id='azureblob_wasb',
+    create_container=True,
+    # retries=3,                               # Reintentos
+    # retry_delay=timedelta(minutes=5),        # Delay entre reintentos
+    # execution_timeout=timedelta(minutes=30)  # Timeout
     )
